14ded37921987f998755b4ab284e8175a415dc2f
Michael Lyon <mlyon360@gmail.com>
2022-06-26 23:41:31 -0600

    add cloudflare pages support

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,11 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ["**/.*"],
+  serverBuildTarget: 'cloudflare-pages',
+  server: './server.js',
+  devServerBroadcastDelay: 1000,
+  ignoredRouteFiles: ['**/.*'],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
+  // serverBuildPath: "functions/[[path]].js",
   // publicPath: "/build/",
-};
+}
